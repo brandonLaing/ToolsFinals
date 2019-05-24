65d8e5998f64f8f95f071e44199ba52fd0fd8f71
svc-student <svc-student@student.directory.jccc>
2019-05-23 20:45:46 -0500

    Finished

---
+++ Assets/AutoAim.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// You may or may not want to create a custom inspector for this class,
+/// depending on your approach to solving the problem.
+/// 
+/// Remember: You are submitting a series of times, rotations, and elevations - not code.
+/// You only need to write a custom inspector if it will be useful to you in 
+/// finding valid shots.
+/// </summary>
+public class AutoAim : MonoBehaviour
+{
+
+  public float[] shotTimes;
+  public float[] platformRotations;
+  public float[] barrelElevations;
+
+  private int shotIndex = 0;
+
+  private BarrelControl barrelControl;
+
+  public List<Vector3> positions = new List<Vector3>();
+  public List<float> timeDifference = new List<float>();
+
+  public GameObject wanderer = null;
+  public BarrelControl aim = null;
+
+  public List<bool> hitTracker = new List<bool>();
+
+  private void Awake()
+  {
+    System.Text.StringBuilder timeSB = new System.Text.StringBuilder();
+    System.Text.StringBuilder rotationSB = new System.Text.StringBuilder();
+    System.Text.StringBuilder elevationSB = new System.Text.StringBuilder();
+
+    for (int i = 0; i < 10; i++ )
+    {
+      timeSB.AppendLine(shotTimes[i].ToString());
+      rotationSB.AppendLine(platformRotations[i].ToString());
+      elevationSB.AppendLine(barrelElevations[i].ToString());
+    }
+
+    Debug.Log(timeSB);
+    Debug.Log(rotationSB);
+    Debug.Log(elevationSB);
+  }
+  void Start()
+  {
+    barrelControl = GetComponent<BarrelControl>();
+  }
+
+  void Update()
+  {
+    if (shotIndex >= shotTimes.Length)
+    {
+      this.enabled = false;
+      return;
+    }
+
+    float shotTime = shotTimes[shotIndex];
+    if (Time.timeSinceLevelLoad >= shotTime)
+    {
+      Debug.Log(wanderer.transform.position);
+
+      // Fire!
+      barrelControl.platformRotation = platformRotations[shotIndex];
+      barrelControl.barrelElevation = barrelElevations[shotIndex];
+
+      Debug.Log($"Time to hit: {aim.GetEstimatedShotLandingTime()}");
+      //if (shotIndex == 9)
+      //Debug.Log(wanderer.transform.position);
+
+      barrelControl.Fire();
+      shotIndex++;
+    }
+
+  }
+  private void OnDrawGizmos()
+  {
+    for (int i = 0; i < positions.Count; i++)
+    {
+      if (hitTracker[i])
+      {
+        Gizmos.color = Color.green;
+      }
+      else
+      {
+        Gizmos.color = Color.red;
+      }
+      Vector3 point = positions[i];
+      Gizmos.DrawWireSphere(point, 1);
+    }
+
+    if (Application.isPlaying)
+    {
+      Gizmos.color = Color.black;
+      try
+      {
+        Gizmos.DrawSphere(positions[shotIndex], 1);
+      }
+      catch
+      {
+
+      }
+    }
+  }
+}
---
+++ Assets/ReportHits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#region Nothing to see here (do not change)
+
+public class ReportHits : MonoBehaviour
+{
+  private bool firstHitReported = false;
+  private void OnCollisionEnter(Collision collision)
+  {
+
+    if (!firstHitReported)
+    {
+      if (collision.collider.tag == "Player")
+      {
+        Debug.LogError("Player hit!");
+      }
+      else
+      {
+        Debug.LogError("Miss");
+      }
+      firstHitReported = true;
+    }
+
+  }
+}
+#endregion
